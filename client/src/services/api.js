43cca20aeee385b82851c9928862ce6a6ca9cde2
Ruby <dorothymwende505@gmail.com>
2026-08-12 16:23:49 +0300

    Fix API base URL with /api path

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://serenity-app-production-1dc1.up.railway.app' || '/api',
+  baseURL: 'https://serenity-app-production-1dc1.up.railway.app',
   headers: {
     'Content-Type': 'application/json'
   }
